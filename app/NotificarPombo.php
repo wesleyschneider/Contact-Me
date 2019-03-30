9b1739cae99f492d0acee32a88f987f4ea258c16
Wesley Schneider <wesleyschneideraires@gmail.com>
2019-03-30 14:46:34 -0300

    Classe para enviar notificação por POMBO, implementada pela interface

---
+++ app/NotificarPombo.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace App;
+
+    class NotificarPombo implements NotificacaoInterface{
+
+        public function notificar(){
+            echo "Enviando Pombo!!!!";
+        }
+
+    }
+
+?>
