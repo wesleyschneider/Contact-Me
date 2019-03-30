4c487c9f3b362dda9a19647d3b26a82a27b4161f
Wesley Schneider <wesleyschneideraires@gmail.com>
2019-03-30 16:52:42 -0300

    Exibição de mensagens de erro (campos obrigatórios)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,16 @@
 
     <hr />
 
+    @if(count($errors) > 0)
+      <div class="alert alert-danger">
+        <ul>
+          @foreach($errors->all() as $error)
+          <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
     <form action="/enviar" method="POST">
       <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
